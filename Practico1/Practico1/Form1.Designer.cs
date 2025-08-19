566e5ab0bfea509521ac212456b4ea8af7b1bd20
gabifirma <gabfirmapaz@gmail.com>
2025-08-18 23:39:33 -0300

    Add files via upload

---
+++ Practico1/Practico1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Practico1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.TApellido = new System.Windows.Forms.TextBox();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.TGrande = new System.Windows.Forms.RichTextBox();
+            this.BSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LApellido.Location = new System.Drawing.Point(44, 79);
+            this.LApellido.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(75, 19);
+            this.LApellido.TabIndex = 0;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LNombre.Location = new System.Drawing.Point(45, 137);
+            this.LNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(74, 19);
+            this.LNombre.TabIndex = 1;
+            this.LNombre.Text = "Nombre";
+            // 
+            // TApellido
+            // 
+            this.TApellido.Location = new System.Drawing.Point(144, 74);
+            this.TApellido.Margin = new System.Windows.Forms.Padding(4);
+            this.TApellido.Multiline = true;
+            this.TApellido.Name = "TApellido";
+            this.TApellido.Size = new System.Drawing.Size(179, 24);
+            this.TApellido.TabIndex = 2;
+            // 
+            // TNombre
+            // 
+            this.TNombre.Location = new System.Drawing.Point(144, 137);
+            this.TNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.TNombre.Multiline = true;
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(179, 24);
+            this.TNombre.TabIndex = 3;
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.Location = new System.Drawing.Point(67, 215);
+            this.BGuardar.Margin = new System.Windows.Forms.Padding(4);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(104, 28);
+            this.BGuardar.TabIndex = 4;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = true;
+            this.BGuardar.Click += new System.EventHandler(this.BGuardar_Click);
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.Location = new System.Drawing.Point(224, 215);
+            this.BEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(100, 28);
+            this.BEliminar.TabIndex = 5;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.UseVisualStyleBackColor = true;
+            this.BEliminar.Click += new System.EventHandler(this.BEliminar_Click);
+            // 
+            // TGrande
+            // 
+            this.TGrande.Location = new System.Drawing.Point(369, 74);
+            this.TGrande.Margin = new System.Windows.Forms.Padding(4);
+            this.TGrande.Name = "TGrande";
+            this.TGrande.Size = new System.Drawing.Size(216, 169);
+            this.TGrande.TabIndex = 7;
+            this.TGrande.Text = "";
+            // 
+            // BSalir
+            // 
+            this.BSalir.Location = new System.Drawing.Point(487, 278);
+            this.BSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.BSalir.Name = "BSalir";
+            this.BSalir.Size = new System.Drawing.Size(100, 28);
+            this.BSalir.TabIndex = 8;
+            this.BSalir.Text = "Salir";
+            this.BSalir.UseVisualStyleBackColor = true;
+            this.BSalir.Click += new System.EventHandler(this.BSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 321);
+            this.Controls.Add(this.BSalir);
+            this.Controls.Add(this.TGrande);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
+            this.Controls.Add(this.TNombre);
+            this.Controls.Add(this.TApellido);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LApellido);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Mi Primer Formulario";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.TextBox TApellido;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button BEliminar;
+        private System.Windows.Forms.RichTextBox TGrande;
+        private System.Windows.Forms.Button BSalir;
+    }
+}
+
---
+++ Practico1/Practico1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practico1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void BGuardar_Click(object sender, EventArgs e)
+        {
+            String nombre = TNombre.Text;
+            String apellido = TApellido.Text;
+
+            TGrande.Text = nombre + " " + apellido;
+        }
+
+        private void BSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BEliminar_Click(object sender, EventArgs e)
+        {
+            TGrande.Text = "";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                this.Close();
+            }
+        }
+    }
+}
